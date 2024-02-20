373adf1be812e610aeab2af54a144fc9ceb100ce
yongwookhong <sinabro.yong@gmail.com>
2024-02-20 22:21:35 +0900

    add files.cs

---
+++ _2_3/_2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("world");
+
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+    }
+}
