d85d42e21b720dae1cfc38919979b718fdec8d0c
yongwookhong <sinabro.yong@gmail.com>
2024-02-20 21:23:56 +0900

    [add] helloworld.cs, helloworld2.cs, writeline.cs

---
+++ Hello_world_2/Hello_world_2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Hello_world_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My Name is Kero");
+
+            Console.WriteLine(10);
+            Console.WriteLine(3.141592);
+            Console.WriteLine(3 + 3);
+
+            Console.WriteLine("hey");
+
+            //wirte는 출력하고 다른줄로 내려오지 않는다
+            Console.Write("Hello! ");
+            Console.Write("We are Learning ");
+            Console.WriteLine("at TeamSparta");
+
+            // \n 은 줄을바꿔줌
+            Console.WriteLine("Hello\nWorld");
+            // 출력결과
+            // Hello
+            // World
+
+            // \t 는 tab을 넣어줌 8칸 띄어준다.
+            Console.WriteLine("Name\tAge");
+            Console.WriteLine("Kero\t30");
+            Console.WriteLine("Young\t25");
+            // 출력결과
+            // Name    Age
+            // Kero    30
+            // Young   25
+
+            // \" 큰따음표 삽입
+            Console.WriteLine("We learn \"C# Programming\"");
+            // 출력결과
+            // The book is called "C# Programming"
+
+            // \' 작은 따음표 삽입
+            Console.WriteLine("He said, \'Hello\' to me.");
+            // 출력결과
+            // He said, 'Hello' to me.
+
+            // \\ 역슬래쉬 넣기 경로 넣을때
+            Console.WriteLine("C:\\MyDocuments\\Project\\");
+            // 출력결과
+            // C:\MyDocuments\Project\
+        }
+    }
+}
---
+++ writeline/writeline/Program.cs
@@ -0,0 +1,22 @@
+﻿using System; //여기서 시스템은 네임스페이스를 의미
+//이걸 함으로써 시스템안에 있는 기능들을 사용하겠다라고 선언
+
+// 네임스페이스는 클래스들의 묶음 내지는 어떠한 기능들의 묶음이라고 생각하면 됨
+namespace HelloWorld
+{
+    internal class Program
+    {
+     
+        // 메인 메서드. 프로그램을 시작했을때 가장 처음으로 호출됨
+        static void Main(string[] args)
+        {
+            //시스템을 using 함으로써 쓸수 있는 코드 Console.WriteLine();
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("C#");
+            Console.WriteLine("study");
+
+            for (int i = 0; i < 10; i++) { }
+        
+        }
+    }
+}
